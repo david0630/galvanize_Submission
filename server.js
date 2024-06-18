da0cbc48f2f7ce0ab1262ddca17001618dcec07e
david0630 <daverspl@gmail.com>
2024-06-18 11:51:00 -0400

    Done

---
+++ server.js
@@ -0,0 +1,19 @@
+//import * as http from 'node:http';
+const http = require('node:http');
+const HOST = 'localhost';
+const PORT = 3000;
+
+const server = http.createServer((request, response) => {
+  let url = request.url.split('/');
+  if (url[1] === '' ) {
+    if (request.method === 'GET') {
+      response.end("Hello Galvanize!");
+    }
+  }
+  response.statusCode = 404;
+  response.end();
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
+});
